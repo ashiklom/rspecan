59a14141c0858b25ea99b9f1c322aae916e690c0
Alexey Shiklomanov <ashiklom@bu.edu>
2018-04-05 13:39:24 -0400

    Major revisions to validation fig script

--- scripts/figures/validation.R
+++ scripts/figures/validation.R
@@ -1,10 +1,13 @@
 library(tidyverse)
 library(rspecan)
+library(metar)
+import::from("knitr", "kable")
 
 figdir <- indir("manuscript", "figures")
 
 results <- read_csv("spectra_db/cleaned_results.csv")
 metadata <- get_metadata("spectra_db")
+species_info <- read_csvy("spectra_db/species_info.csvy")
 
 project_colors <- read_csv("spectra_db/project_colors.csv") %>%
   df2dict("color", "short_name")
@@ -13,6 +16,7 @@ md_sub <- metadata %>%
   select(
     project_code, short_name,
     observation_id,
+    species_code,
     leaf_chltot_per_area,
     leaf_cartot_per_area,
     leaf_water_thickness,
@@ -23,61 +27,98 @@ md_sub <- metadata %>%
 dat <- left_join(results, md_sub) %>%
   filter(!is.na(short_name))
 
-valid_plot <- function(specparam, trueparam) {
-  dat_sub <- filter(dat, parameter == specparam, !is.na(trueparam))
-  ggplot(dat_sub) +
-    aes_string(x = "Mean", y = trueparam, color = "short_name") +
-    geom_point() +
-    geom_smooth(method = "lm", se = FALSE) +
-    geom_smooth(mapping = aes(group = 1), method = "lm", se = FALSE,
-                linetype = "dashed", color = "black") +
-    facet_wrap(~prospect_version, scales = "free") +
-    theme_bw() +
-    scale_color_manual(values = project_colors)
-}
+specparam <- "Cab"
+trueparam <- "leaf_chltot_per_area"
 
-valid_plot("Cab", "leaf_chltot_per_area")
-ggsave(infile(figdir, "validation.Cab.pdf"))
-valid_plot("Car", "leaf_cartot_per_area") + coord_cartesian(xlim = c(0, 40))
-ggsave(infile(figdir, "validation.Car.pdf"))
-valid_plot("Cw", "leaf_water_thickness") + coord_cartesian(xlim = c(0, 0.11), ylim = c(0, 0.06))
-ggsave(infile(figdir, "validation.Cw.pdf"))
-valid_plot("Cm", "leaf_mass_per_area") + coord_cartesian(xlim = c(0, 0.1), ylim = c(0, 0.04))
-ggsave(infile(figdir, "validation.Cm.pdf"))
-
-############################################################
-
-old_valid_plot <- function(dat, specparam, trueparam) {
-
-  x <- paste0(specparam, "_mid")
-  xmin <- paste0(specparam, "_lo")
-  xmax <- paste0(specparam, "_hi")
-
-  specparam_q <- rlang::sym(x)
+validate <- function(specparam, trueparam, coords_list = list()) {
   trueparam_q <- rlang::sym(trueparam)
   dat_sub <- dat %>%
-    filter(!is.na(!!specparam_q), !is.na(!!trueparam_q))
+    filter(parameter == specparam, !is.na(!!trueparam_q)) %>%
+    rename(lo = `2.5%`, hi = `97.5%`) %>%
+    mutate(error = abs(Mean - !!trueparam_q))
+  plt <- ggplot(dat_sub) +
+    aes_string(x = "Mean", xmin = "lo", xmax = "hi",
+               y = trueparam, color = "short_name") +
+    geom_point(size = 0.5) +
+    geom_errorbarh(color = "grey50", size = 0.5, alpha = 0.5) +
+    geom_smooth(method = "lm", se = FALSE) +
+    geom_smooth(mapping = aes(group = 1), method = "lm", se = FALSE,
+                linetype = "solid", color = "black") +
+    geom_abline(linetype = "dashed") +
+    facet_wrap(~prospect_version, scales = "free") +
+    do.call(coord_cartesian, coords_list) +
+    theme_bw() +
+    scale_color_manual(values = project_colors)
+  ggsave(infile(figdir, paste0("validation.", specparam, ".pdf")), plt)
 
-  lm_form <- as.formula(paste(trueparam, x, sep = " ~ "))
-  dat_mod <- dat_sub %>%
-    group_by(project) %>%
+  lm_form <- paste(trueparam, "~", "Mean")
+  lm_byproject <- dat_sub %>%
+    group_by(short_name) %>%
     nest() %>%
     mutate(
-      lmfit = map(data, ~lm(lm_form, data = .)),
+      lmfit = map(data, ~lm(formula(lm_form), data = .)),
+      coefs = map(lmfit, coef),
+      slope = map_dbl(coefs, "Mean"),
+      intercept = map_dbl(coefs, "(Intercept)"),
       r2 = map2_dbl(lmfit, data, modelr::rsquare),
-      label = map2_chr(project, r2, ~sprintf("%s: %.02f", .x, .y))
+      mae = map(data, "error") %>% map_dbl(mean, na.rm = TRUE)
     ) %>%
-    arrange(desc(r2))
+    select(
+      `Project` = short_name,
+      `Slope` = slope,
+      `Intercept` = intercept,
+      `R2` = r2,
+      `MAE` = mae
+    )
 
-  lab <- paste(dat_mod$label, collapse = "\n")
+  kable(lm_byproject, format = "latex") %>%
+    cat(file = infile(figdir, paste0("r2_byproject.", specparam, ".tex")))
 
-  ggplot(dat_sub) +
-    aes_string(x = x, xmin = xmin, xmax = xmax, y = trueparam, color = "project") +
-    geom_errorbarh(color = "grey50", size = 0.5, alpha = 0.5) +
-    geom_point(size = 0.5) +
-    geom_smooth(aes(group = 1), method = "lm", se = FALSE, color = "black") +
-    geom_smooth(method = "lm", se = FALSE) +
-    geom_abline(linetype = "dashed") +
-    annotate("text", x = Inf, y = Inf, label = lab, hjust = 1, vjust = 1)
+  err_byspecies <- dat_sub %>%
+    group_by(short_name, species_code, prospect_version) %>%
+    summarize(mae = mean(error, na.rm = TRUE)) %>%
+    ungroup() %>%
+    select(short_name, species_code, prospect_version, mae) %>%
+    mutate(
+      sp_proj = interaction(species_code, short_name, prospect_version) %>%
+        fct_reorder(mae)
+    )
+
+  overall_err <- dat_sub %>%
+    group_by(prospect_version) %>%
+    summarize(mae = mean(error, na.rm = TRUE))
+
+  ggplot(err_byspecies) +
+    aes(x = sp_proj, y = mae, fill = short_name) +
+    geom_col() +
+    geom_hline(aes(yintercept = mae), data = overall_err, linetype = "dashed", color = "black") +
+    facet_wrap(~prospect_version, scales = "free_x") +
+    xlab("Species x Project") +
+    ylab("Mean absolute error") +
+    scale_fill_manual(values = project_colors) +
+    theme_bw() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank()
+    ) -> plt
+
+  ggsave(infile(figdir, paste0("r2_speciesbyproj.", specparam, ".pdf")), plt)
+
+  lm_bsi <- err_byspecies %>%
+    left_join(species_info) %>%
+    filter(!is.na(leaf_type))
+
+  lt_colors <- c(broad = "green4", needle = "yellow2")
+
+  plt %+% lm_bsi +
+    aes(fill = leaf_type) +
+    scale_fill_manual(values = lt_colors) -> plt
+    #scale_fill_brewer(type = "qual") -> plt
+
+  ggsave(infile(figdir, paste0("r2_speciesbyleaf.", specparam, ".pdf")), plt)
 }
 
+validate("Cab", "leaf_chltot_per_area")
+validate("Car", "leaf_cartot_per_area", list(xlim = c(0, 40)))
+validate("Cw", "leaf_water_thickness", list(xlim = c(0, 0.11), ylim = c(0, 0.06)))
+validate("Cm", "leaf_mass_per_area", list(xlim = c(0, 0.1), ylim = c(0, 0.04)))
